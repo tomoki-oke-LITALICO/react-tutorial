3de589ecc772ae6a5b27bfb94c089297cb80f01a
Tomoki Oke(LITALICO) <tomoki.oke@litalico.co.jp>
2019-07-18 10:05:10 +0900

    :heavy_plus_sign: 三目並べで勝敗を表示できるまで完成

--- src/board.tsx
+++ src/board.tsx
@@ -6,15 +6,61 @@ import { Square } from './square'
 export const Board = () => {
 
     const [squares, setSquares] = useState(Array(9).fill(null));
-    const [status, setStatus] = useState('Next Player: X');
+    const [xIsNext, setXIsNext] = useState(true);
+
+    const handleClick = (i: number) => {
+        const copiedSquares = squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        copiedSquares[i] = xIsNext ? 'X' : 'O';
+        setSquares(copiedSquares);
+        setXIsNext(!xIsNext);
+    };
+
+    const calculateWinner = (squares: string[]) => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (
+                squares[a] &&
+                squares[a] === squares[b] &&
+                squares[a] === squares[c]
+            ) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
 
     const RenderSquare = ( props: { i: number } ) => {
-        return <Square value={ props.i } /> 
+        return <Square
+            value={ squares[props.i] }
+            onClick={ () => { handleClick(props.i) } } /> 
+    };
+
+    const RenderStatus = () => {
+        const winner = calculateWinner(squares);
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+        }
+        return <div className="status"> { status } </div>
     };
 
     return (
         <>
-            <div className="status">{ status }</div>
+            <RenderStatus />
             <div className="board-row">
                 <RenderSquare i={ 0 } />
                 <RenderSquare i={ 1 } />
--- src/square.tsx
+++ src/square.tsx
@@ -1,13 +1,7 @@
 import * as React from 'react';
-import { useState } from 'react';
 
-export const Square = ( props: { value: number } ) => {
-
-    const [value, setValue] = useState(null);
-
-    return (
-        <button className="square" onClick={() => { setValue('X'); }}>
-            { value }
-        </button>
-    );
-};
+export const Square = ( props: { value: number, onClick: () => void } ) => (
+    <button className="square" onClick={ props.onClick }>
+        { props.value }
+    </button>
+);
